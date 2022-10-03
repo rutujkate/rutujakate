9bbeb1b12895c6931df2383946a916e00c75a1d0
rutujkate <114908768+rutujkate@users.noreply.github.com>
2022-10-03 14:05:10 +0530

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -3,5 +3,11 @@ if a>0:
   print("a is positive")
   else:
     print("a is negative")
-    print ("end program")
+print ("end program")
+
+a=int(input("enter number")
+b=int(input("enter number")
+print("sum of both numbers=",a+b)        
+       
+   
     
